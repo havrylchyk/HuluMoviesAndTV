21633f5a10262c9a8456f226436202f24941ab1b
Anastasiia Havrylchyk <anastasiia.havrylchyk@oa.edu.ua>
2023-05-29 01:02:15 +0300

    First commit

---
+++ HuluMoviesAndTVParse/Dtos/MoviesAndTVShowsReadDto.cs
@@ -0,0 +1,65 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System.Text.Json;
+
+namespace HuluMoviesAndTVParse.Dtos
+{
+    public class MoviesAndTVShowsReadDto
+    {
+        //								
+        [Name("type")]
+        public string Type { get; set; }
+
+        [Name("title")]
+        public string Title { get; set; }
+
+        [Name("director")]
+        public string? Director { get; set; }
+
+        [Name("country")]
+        public string? Country { get; set; }
+
+        [Name("release_year")]
+        public string ReleaseYear { get; set; }
+
+        [Name("rating")]
+        public string? Rating { get; set; }
+
+        [Name("duration")]
+        public string? Duration { get; set; }
+
+        [Name("listed_in")]
+        public string ListedIn { get; set; }
+
+        [Name("description")]
+        public string Description { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Type} | {Title} | {Director} | {Country} | {ReleaseYear} | " +
+                $"{Rating} | {Duration} | {ListedIn} | {Description}";
+        }
+
+        //public string[] CountryParsed
+        //{return $"{Type} | {Title} | {Director} | {Country} | {ReleaseYear} | {Rating} | {Duration} | {ListedIn} | {Description}";
+        //    get
+        //    {
+        //        return string.IsNullOrEmpty(Team)
+        //            ? new string[0]
+        //            : JsonSerializer.Deserialize<string[]>(Team.Replace("\'", "\""));
+        //    }
+        //}
+
+        //public string Genres { get; set; }
+
+        //public string[] GenresParsed
+        //{
+        //    get
+        //    {
+        //        return string.IsNullOrEmpty(Genres)
+        //            ? new string[0]
+        //            : JsonSerializer.Deserialize<string[]>(Genres.Replace("\'", "\""));
+        //    }
+        //}
+    }
+}
---
+++ HuluMoviesAndTVParse/Program.cs
@@ -0,0 +1,109 @@
+﻿
+using CsvHelper;
+using HuluMoviesAndTVParse.Dtos;
+//using GamesHistory.Core.Context;
+//using GamesHistory.ParserApp.Dtos;
+//using GamesHistory.Core.Entities;
+
+////throw new ArgumentException("---- - - -");
+
+Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} -- APP STARTED --");
+
+//using (var ctx = new GamesContext())
+//{
+//    ctx.Database.EnsureDeleted();
+//    ctx.Database.EnsureCreated();
+
+using (var reader = new StreamReader("HuluTitles.csv"))
+using (var csvReader = new CsvReader(reader, culture: System.Globalization.CultureInfo.CurrentCulture))
+{
+       var records = csvReader.GetRecords<MoviesAndTVShowsReadDto>().ToList();
+    //int row = 0;
+    //foreach (var record in records.Take(3072))
+    //{
+    //    Console.WriteLine($"{++row}: {record}");
+    //}
+    //if (records.Count > 0)
+    //{
+    //    var firstRecord = records[0];
+    //    Console.WriteLine(firstRecord);
+    //}
+    var firstRecord = records.FirstOrDefault();
+    Console.WriteLine(firstRecord);
+
+    //        int row = 0;
+
+    //        List<string> teams = new List<string>();
+    //        List<string> genres = new List<string>();
+
+
+
+    //foreach (var r in records)
+    //{
+    //    if (r.TeamsParsed.Length > 0)
+    //        teams.AddRange(r.TeamsParsed);
+    //    if (r.GenresParsed.Length > 0)
+    //        teams.AddRange(r.GenresParsed);
+    //}
+
+    //        ctx.Teams.AddRange(teams.Distinct().Select(x => new Team
+    //        {
+    //            Title = x
+    //        }));
+
+    //        ctx.Genres.AddRange(teams.Distinct().Select(x => new Genre
+    //        {
+    //            Title = x
+    //        }));
+
+    //        ctx.SaveChanges();
+    //        Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} -- TEAMS AND GENRES ADDED --");
+
+
+    //        var games = new List<Game>();
+
+    //        foreach (var x in records)
+    //        {
+    //            var game = new Game
+    //            {
+    //                NumberOfReviews = ParseNumbersWithLetters(x.NumberOfReviews),
+    //                Plays = ParseNumbersWithLetters(x.Plays),
+    //                Rating = x.Rating,
+    //                ReleaseDate = x.RealeaseDate,
+    //                Reviews = new List<Review>() { new Review { Text = x.Reviews } },
+    //                Summary = x.Summary,
+    //                TimesListed = ParseNumbersWithLetters(x.TimesListed),
+    //                Wishlist = ParseNumbersWithLetters(x.Wishlist),
+    //                Title = x.Title
+    //            };
+
+    //            if (x.TeamsParsed.Length > 0)
+    //                foreach (var t in x.TeamsParsed)
+    //                    game.Teams.Add(ctx.Teams.First(x => x.Title == t));
+
+    //            if (x.GenresParsed.Length > 0)
+    //                foreach (var t in x.GenresParsed)
+    //                    game.Genre.Add(ctx.Genres.First(x => x.Title == t));
+
+    //            games.Add(game);
+    //        }
+    //        ctx.Games.AddRange(games);
+    //        ctx.SaveChanges();
+    //        Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} -- GAMES ADDED --");
+    //    }
+}
+
+//static int ParseNumbersWithLetters(string textNumber)
+//{
+//    if (string.IsNullOrEmpty(textNumber)) return 0;
+
+//    double num; //5.5K // 985
+
+//    if (!double.TryParse(textNumber, out num))
+//    {
+//        textNumber = textNumber.ToUpper().Trim().TrimEnd('K');
+//        num = double.Parse(textNumber) * 1000;
+//    }
+
+//    return (int)num;
+//}
