1870084d113d060ae2dec34730bc2207e07baab6
Anastasiia Havrylchyk <anastasiia.havrylchyk@oa.edu.ua>
2023-05-29 03:47:44 +0300

    Fourth commit

--- HuluMoviesAndTV.Core/Context/MoviesAndShowsContext.cs
+++ HuluMoviesAndTV.Core/Context/MoviesAndShowsContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HuluMoviesAndTV.Core.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,22 @@ using System.Threading.Tasks;
 
 namespace HuluMoviesAndTV.Core.Context
 {
-    internal class MoviesAndShowsContext
+    public class MoviesAndShowsContext: DbContext
     {
+        public DbSet<MovieAndShow> MovieAndShows => Set<MovieAndShow>();
+        public DbSet<Country> Countries => Set<Country>();
+        public DbSet<Director> Directors => Set<Director>();
+        public DbSet<Listened> Listeneds => Set<Listened>();
+        public DbSet<ProgramType> ProgramTypes => Set<ProgramType>();
+        public DbSet<Rating> Ratings => Set<Rating>();
+        public DbSet<ReleaseYear> ReleaseYears => Set<ReleaseYear>();
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=HuluDb;Trusted_Connection=True;TrustServerCertificate=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
     }
 }
