b4cce718ba4b3660eb3537d9cf52521a271750b2
Anastasiia Havrylchyk <anastasiia.havrylchyk@oa.edu.ua>
2023-05-30 04:07:08 +0300

    7 commit

--- HuluMoviesAndTV.Test/Program.cs
+++ HuluMoviesAndTV.Test/Program.cs
@@ -9,13 +9,17 @@ repository.Create(new MovieAndShow
     Title = "A new Title",
     Duration = "30minutes",
     Description = "some description",
+    TypeId = Guid.Parse("7873F208-05FA-46F3-9F9A-0010F9CE1985"),
+    YearId = Guid.Parse("DC77DE1B-E2A8-47FD-B710-0019DFE6E13B"),
+    DirectorId = Guid.Parse("F3F77B94-0796-454B-9726-27B6A15B8612"),
+    RatingId = Guid.Parse("86B6356E-B6BE-4E2F-9C00-026E866115E2"),
+}) ;
 
-});
+//Console.WriteLine($"Count: {repository.GetAll().Count()}");
 
-Console.WriteLine($"Count: {repository.GetAll().Count()}");
 
 
 foreach (var movieAndShow in repository.GetAll().OrderBy(x => x.Title))
 {
-    Console.WriteLine($"Game: {movieAndShow.Title}");
+    Console.WriteLine($"MovieAndShow: {movieAndShow.Title}");
 }
--- HuluMoviesAndTVParse/Program.cs
+++ HuluMoviesAndTVParse/Program.cs
@@ -3,7 +3,10 @@ using HuluMoviesAndTV.Core.Context;
 using HuluMoviesAndTV.Core.Entity;
 using HuluMoviesAndTVParse.Dtos;
 
+//throw new ArgumentException("---- - - -");
+
 Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} -- APP STARTED --");
+
 
 using (var ctx = new MoviesAndShowsContext())
 {
@@ -55,11 +58,44 @@ using (var ctx = new MoviesAndShowsContext())
                 Title = x.Title,
                 Duration = x.Duration,
                 Description = x.Description,
-                Type = new ProgramType { Name = x.Type },
-                Year = new ReleaseYear { Year = x.ReleaseYear },
                 Country = new List<Country>(),
                 Listened = new List<Listened>()
             };
+
+
+            movieAndShow.Type = ctx.ProgramTypes.FirstOrDefault(pt => pt.Name == x.Type);
+            if (movieAndShow.Type == null)
+            {
+                movieAndShow.Type = new ProgramType { Name = x.Type };
+                ctx.ProgramTypes.Add(movieAndShow.Type);
+            }
+            movieAndShow.Year = ctx.ReleaseYears.FirstOrDefault(ry => ry.Year == x.ReleaseYear);
+            if (movieAndShow.Year == null)
+            {
+                movieAndShow.Year = new ReleaseYear { Year = x.ReleaseYear };
+                ctx.ReleaseYears.Add(movieAndShow.Year);
+            }
+
+            if (!string.IsNullOrEmpty(x.Director))
+            {
+                movieAndShow.Director = ctx.Directors.FirstOrDefault(d => d.Name == x.Director);
+                if (movieAndShow.Director == null)
+                {
+                    movieAndShow.Director = new Director { Name = x.Director };
+                    ctx.Directors.Add(movieAndShow.Director);
+                }
+            }
+
+            if (!string.IsNullOrEmpty(x.Rating))
+            {
+                movieAndShow.Rating = ctx.Ratings.FirstOrDefault(r => r.Name == x.Rating);
+                if (movieAndShow.Rating == null)
+                {
+                    movieAndShow.Rating = new Rating { Name = x.Rating };
+                    ctx.Ratings.Add(movieAndShow.Rating);
+                }
+            }
+
             if (x.CountryParsed.Length > 0)
                 foreach (var t in x.CountryParsed)
                     movieAndShow.Country.Add(ctx.Countries.Distinct().First(x => x.Name == t));
@@ -67,28 +103,6 @@ using (var ctx = new MoviesAndShowsContext())
             if (x.ListedInParsed.Length > 0)
                 foreach (var t in x.ListedInParsed)
                     movieAndShow.Listened.Add(ctx.Listeneds.Distinct().First(x => x.Name == t));
-
-            if (!string.IsNullOrEmpty(x.Director))
-            {
-                var director = ctx.Directors.Distinct().FirstOrDefault(d => d.Name == x.Director.Trim());
-                if (director == null)
-                {
-                    director = new Director { Name = x.Director };
-                    ctx.Directors.Add(director);
-                }
-                movieAndShow.Director = director;
-            }
-
-            if (!string.IsNullOrEmpty(x.Rating))
-            {
-                var rating = ctx.Ratings.Distinct().FirstOrDefault(r => r.Name == x.Rating.Trim());
-                if (rating == null)
-                {
-                    rating = new Rating { Name = x.Rating };
-                    ctx.Ratings.Add(rating);
-                }
-                movieAndShow.Rating = rating;
-            }
 
             ctx.SaveChanges();
 
